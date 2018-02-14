c11a2df40766a4ca93bef5574fcbd4fd2ff16e55
Neto <jose.lopes.santos.neto@gmail.com>
2018-02-14 23:43:17 +0100

    Deploy no heroku

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,8 +5,15 @@ class HomeController < ApplicationController
 
   def emite_boletos
     xls_file = params[:file].path
-    boleto_simples_service = Boleto.new
-    @logs = boleto_simples_service.create_from_xls(xls_file)
+    token = params[:token]
+    if token && xls_file
+      session['BOLETO_TOKEN'] = params[:token]
+      boleto_simples_service = Boleto.new(token)
+      @logs = boleto_simples_service.create_from_xls(xls_file)
+    else
+      flash[:error] = 'Informe o arquivo e o Token'
+      redirect_to home_index_path
+    end
   rescue => e
     flash[:error] = e.message
     redirect_to home_index_path
--- app/services/boleto.rb
+++ app/services/boleto.rb
@@ -3,10 +3,10 @@ require 'roo'
 
 class Boleto
 
-  def initialize
+  def initialize(token)
     BoletoSimples.configure do |c|
       c.environment = ENV['BOLETOSIMPLES_ENV'].to_sym
-      c.access_token = ENV['BOLETOSIMPLES_ACCESS_TOKEN']
+      c.access_token = (token || ENV['BOLETOSIMPLES_ACCESS_TOKEN'])
     end
   end
 
@@ -15,20 +15,23 @@ class Boleto
     workbook.default_sheet = workbook.sheets[0]
     boletos = []
     ((workbook.first_row + 1)..workbook.last_row).each do |row|
+      cep = workbook.row(row)[7].to_s
+      phone = workbook.row(row)[5].to_s.try(:gsub,'(','').try(:gsub,')','')
       boletos << {
           description: workbook.row(row)[0],
           amount: workbook.row(row)[1],
           expire_at: workbook.row(row)[2],
           customer_person_name: workbook.row(row)[3],
           customer_cnpj_cpf: workbook.row(row)[4],
-          customer_phone_number: workbook.row(row)[5],
+          customer_phone_number: phone,
           customer_email: workbook.row(row)[6],
-          customer_zipcode: workbook.row(row)[7],
+          customer_zipcode: cep,
           customer_address: workbook.row(row)[8],
           customer_address_number: workbook.row(row)[9],
           customer_address_complement: workbook.row(row)[10],
           customer_neighborhood: workbook.row(row)[11],
           customer_city_name: workbook.row(row)[12],
+          customer_state: ('SP' ||  workbook.row(row)[14]),
           customer_person_type: 'individual',
       }
     end
@@ -45,15 +48,15 @@ class Boleto
   # Criar um boleto passando os dados dos clientes
   # Não vamos salvar clientes pois os dados podem mudar
   def create(boleto_hash)
-    @bank_billet = BoletoSimples::BankBillet.create(boleto_hash)
-    if @bank_billet.persisted?
-      puts "Sucesso :)"
-      puts @bank_billet.attributes
+    bank_billet = BoletoSimples::BankBillet.create(boleto_hash)
+    if bank_billet.persisted?
+      response = bank_billet.attributes
     else
-      puts "Erro :("
-      puts @bank_billet.response_errors
+      response = bank_billet.response_errors
+      response[:dados_informados] = boleto_hash
     end
-    @bank_billet
+    puts response
+    response
   rescue => e
     puts e.message
     e.message
